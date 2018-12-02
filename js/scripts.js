bac4d11268cb0d0d15cd7e2eabb535a2155141ae
Ian Kabugi <iank299@gmail.com>
2018-12-03 02:01:31 +0300

    style your body

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+var sides=function(){
+var a=parseInt(prompt("enter length a"));
+var a=parseInt(prompt("enter length b"));
+var b=parseInt((prompt("enter length c"));
+
+if (a===integer b===integer c===integer) {
+  return
+} else {
+  alert("not an integer")
+}
+}
